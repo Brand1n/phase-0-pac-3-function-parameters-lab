f3249e049b6ac9be97c76b88cb697beecbbb7bff
Brandon Tucker <Brandon064@outlook.com>
2023-10-26 01:13:00 -0700

    perameters

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 function introduction(name){
-     return `Hi, my name is ${name}.`
+    return (`Hi, my name is ${name}.`)
 }
+function introductionWithLanguage(name, language) {
+  
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+  }
 
-function introductionWithLanguage(name, language){
+  function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return(`Hi, my name is ${name} and I am learning to program in ${language}.`)
+  }
 
-console.log('Hi, my name is ${name} and I am learning to program in ${language}.')
-    console.log(name);
-    console.log(language);
+  
+  
 
-}
-introductionWithLanguage("Alki", "Ember.js");
-
-function introductionWithLanguageOptional(name,language){
-    if(language === JavaScript){
-        return "Hi, my name is " + name + ".";
-    } else {
-        return "Hi, my name is " + name + " and I am lerarning to  program in " + language + ".";
-    }
-    }
-    function introductionWithLanguageOptional(name, language){
-        if(language === Python)
-          return "Hi, my name is " + name + ".";
-      } else {
-      return "Hi, my name is " + name + " and I am learning to program in " + language + ".";
-      }
+  
